4ea7b42e7f25fefa1f3689ca5dbfa8ee0f6b2eb3
admin <admin@YD-14>
2022-03-22 17:45:44 +0900

    0322 commit2

--- P20220321/src/com/edu/api/WrapperExample.java
+++ P20220321/src/com/edu/api/WrapperExample.java
@@ -7,10 +7,13 @@ public class WrapperExample {
         
 		/// 기본타입(int, long, double, boolean....)
 		/// 참조타입(Integer, Long, Double, boolean....)
-		
+		/// int -> Integer (boxing), Integer -> int (unboxing)
 		ArrayList<Integer> ary = new ArrayList<Integer>();
 		ary.add(100);
-		ary.add(new Integer(200));
+		ary.add(new Integer(200)); // Deprecated, 향후자바에서 제거할 예정.
+		
+		
+		new Integer(100).floatValue(); // double type으로 변화, 100.0
 		
 		
 		
