f863ec1eb3062c5c10f497e422008b013093aa7c
admin <admin@YD-14>
2022-03-23 18:52:43 +0900

    보충22

--- P20220321/src/com/homework/Exam01.java
+++ P20220321/src/com/homework/Exam01.java
@@ -2,14 +2,13 @@ package com.homework;
 
 public class Exam01 {
 	public static void main(String[] args) {
-		int num1, num2, result = 0;
+		int num1, num2, result;
 		num1 = 25;
 		num2 = 30;
 		if (num1 > num2) {
 			result = num1 - num2;
 
-		}
-		if (num2 > num1) {
+		} else {
 			result = num2 - num1;
 		}
 		System.out.println("result: " + result);
